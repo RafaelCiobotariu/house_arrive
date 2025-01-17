cf06af3c16fb146eca19c31bbb94dc64ad39d3f7
Rafael Ciobotariu <rafael.ciobotariu@yahoo.com>
2025-01-17 13:09:34 +0200

    initial project + login page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import "./App.css";
+import LoginPage from "./components/login/login.component";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LoginPage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/login/login.component.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./login.style.css";
+import GoogleButton from "react-google-button";
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <div className="login-header">
+          <h1 className="title">HouseArrive</h1>
+          <p className="under-title">
+            For the full experience, <span>log in</span>
+          </p>
+        </div>
+        <div className="login-form-container">
+          <form className="login-form">
+            <label for="email" className="form-label">
+              E-mail
+            </label>
+            <input
+              placeholder="email"
+              type="text"
+              id="email"
+              name="email"
+            ></input>{" "}
+            <label for="password" className="form-label">
+              Password
+            </label>
+            <input
+              placeholder="password"
+              type="password"
+              id="password"
+              name="password"
+            ></input>
+            <input className="submit-button" type="submit" value="Connect" />
+          </form>
+        </div>
+        <GoogleButton
+          label="Continue with Google"
+          type="lights"
+          className="google-button"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
